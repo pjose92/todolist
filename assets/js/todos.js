54a8255bb810dd676ac8e8776062e5659296e24e
Jose <pjose14.jp38@yahoo.com>
2019-02-20 21:34:52 -0600

    first upload

---
+++ assets/js/todos.js
@@ -0,0 +1,28 @@
+//check off specific todos by clicking
+$("ul").on("click", "li", function(){
+	$(this).toggleClass("completed");
+});
+
+//click on X to delete Todos
+$("ul").on("click", "span", function(event){
+	$(this).parent().fadeOut(500,function(){
+		$(this).remove();
+	});
+	//stopping from bubbling to other elements
+	event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+	if(event.which === 13){
+	  //grabbing new todo text from input
+	  var todoText = $(this).val();
+	  	$(this).val("");
+	  //creat a new li and add to ul
+	  $("ul").append("<li><span><i class='fas fa-trash'></i></span> " + todoText + "</li>")
+	}
+
+});
+
+$(".fa-plus").click(function(){
+	$("input[type='text']").fadeToggle()
+});
